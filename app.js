ec79fa82a12ba40562ec95591835d1b8ba926609
mirawinkel <mirawinkel206@gmail.com>
2021-02-10 00:51:32 -0800

    buttons, main cards, images

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button) => {
+  let target = button.querySelector('.target');
+  function handleMove(e) {
+    const x = -50 + (e.pageX - button.offsetLeft - 300 / 2) / 3;
+    const y = -10 + (e.pageY - button.offsetTop - 100 / 2) / 3;
 
-function carouselNormalization() {
-    var items = $('#carousel-example-generic .item'), //grab all slides
-        heights = [], //create empty array to store height values
-        tallest; //create variable to make note of the tallest slide
-    
-    if (items.length) {
-        function normalizeHeights() {
-            items.each(function() { //add heights to array
-                heights.push($(this).height()); 
-            });
-            tallest = Math.max.apply(null, heights); //cache largest value
-            items.each(function() {
-                $(this).css('min-height',tallest + 'px');
-            });
-        };
-        normalizeHeights();
-    
-        $(window).on('resize orientationchange', function () {
-            tallest = 0, heights.length = 0; //reset vars
-            items.each(function() {
-                $(this).css('min-height','0'); //reset min-height
-            }); 
-            normalizeHeights(); //run it again 
-        });
-    }
-    }
-    $(document).ready(carouselNormalization())
+    target.style.setProperty('--x', `${ x }px`)
+    target.style.setProperty('--y', `${ y }px`)
+  }
+  button.addEventListener('mousemove', (e) => {
+    handleMove(e);
+  });
+  button.addEventListener('touchmove', (e) => {
+    handleMove(e.changedTouches[0]);
+  });
+});
